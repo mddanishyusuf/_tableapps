a37781b8bb369ebd7700f1c97fef083a9fb9318a
mohd danish <danish@mohds-iMac.local>
2022-11-24 20:34:38 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+      const [supabase] = useState(() => createBrowserSupabaseClient());
 
-export default MyApp
+      return (
+            <SessionContextProvider
+                  supabaseClient={supabase}
+                  initialSession={pageProps.initialSession}
+            >
+                  <Component {...pageProps} />
+            </SessionContextProvider>
+      );
+}
+export default MyApp;
